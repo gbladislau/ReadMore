97e0a78c8981ddcf70c40a96067b80df80c22000
braga-bilads <gabriel.b.ladislau@gmail.com>
2023-03-30 09:28:49 -0300

    mudando nome para ReadMore

--- readmore/urls.py
+++ readmore/urls.py
@@ -1,4 +1,4 @@
-"""bookshelf URL Configuration
+"""readmore URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
