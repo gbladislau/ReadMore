a4fdc89ffff5c408cc5828f53f6d3f42739699de
braga-bilads <gabriel.b.ladislau@gmail.com>
2023-05-08 11:37:34 -0300

    Search Book Ok, falta passar para DF

--- readmore/API_manager.py
+++ readmore/API_manager.py
@@ -1,5 +1,7 @@
 import requests as rq
 import json as js
+import pandas as pd
+
 class API_man:
     
     def __init__(self, base_url=u'https://openlibrary.org') -> None:
@@ -12,12 +14,16 @@ class API_man:
         return response
     
     def search_book(self,search_key, flags):
-        server_response = self.get_response_from_OPL()
-        return 
-    
-    def transform_to_json(self,response:rq.Response):
-        try:
-            data = response.json()
-        except:
-            print("ERRO: trasnformação da resposta do servidor para JSON falhou")
-        return data
+        server_response = self.get_response_from_OPL('/search.json?q='+search_key.replace(' ','+').removesuffix('\n')+flags)
+        books_results = json_results_to_df(transform_to_json(server_response))
+        return books_results ##DESCOBRIR QUAIS CHAVES PRECISAM SER RETORNADAS
+
+def json_results_to_df(json_dict):
+    return
+
+def transform_to_json(response:rq.Response):
+    try:
+        data = response.json()
+    except:
+        print("ERRO: trasnformação da resposta do servidor para JSON falhou")
+    return data
