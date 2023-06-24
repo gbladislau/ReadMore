20082f9766b899de21e04d0c8aa11d56b0c95fed
Marcus Louriçal F <malnf14@gmail.com>
2023-06-24 10:56:47 -0300

    Delete authentication directory

--- authentication/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('', views.home, name="home"),
-    path('singup', views.signup, name="singup"),
-    path('singin', views.singin, name="singin"),
-    path('singout', views.singout, name="singout")
-]
