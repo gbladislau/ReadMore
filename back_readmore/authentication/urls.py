d76672c81ff6321fab81c06758d86d0a1fa23a39
Marcus Louriçal F <malnf14@gmail.com>
2023-06-24 10:57:12 -0300

    Add files via upload

--- back_readmore/authentication/urls.py
+++ back_readmore/authentication/urls.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-
+    path('', views.home, name="home"),
+    path('singup', views.signup, name="singup"),
+    path('singin', views.singin, name="singin"),
+    path('singout', views.singout, name="singout")
 ]
