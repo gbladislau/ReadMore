9ccb9f82c848b22296fa39f7958b83c7897e004d
MarcusLNF <malnf14@gmail.com>
2023-07-07 22:56:30 -0300

    mudanças na AuthorPage

--- front-readmore/screens/AuthorPage.js
+++ front-readmore/screens/AuthorPage.js
@@ -15,9 +15,83 @@ export default function AuthorPage({ route, navigator }) {
 
   const authorKey = authorData['key']
   const authorName = authorData['name']
-  const authorBio = authorData['bio']['value']
+  const authorBio = authorData.bio && authorData.bio.value;
 
-  // var authorBookData = `https://openlibrary.org/authors/${authorKey}/works.json`
+
+
+  const fetchauthorbooksData = async () => {
+    console.log('Busca livros do autor ');
+    console.log(authorData['key']);
+    const url_ = `https://openlibrary.org${authorData['key']}/works.json?offset=10`;
+    try {
+      setLoading(true)
+      var response = await fetch(url_);
+      if (response.ok) {
+        console.log(`Sucesso na requisição GET para ${url_} HTTP ${response.status}`);
+
+        try {
+          var responseJSON = await response.json();
+          setSearchResults(responseJSON);
+          //console.log(JSON.stringify(responseJSON))
+        }
+        catch (e) {
+          console.log(e)
+          setLoading(false);
+        }
+      }
+      else {
+        console.log(`Falha na requisição GET para ${url_} HTTP ${response.status}`);
+      }
+      setLoading(false);
+    }
+    catch (erro) {
+      console.log(`Erro no GET em ${url_}`);
+      console.log(erro);
+      setLoading(false);
+      Alert.alert("Verifique sua conexão com a internet!");
+    }
+  }
+
+  useEffect(() => {
+    fetchBookData();
+  }, []);
+
+  useEffect(() => {
+    if (searchResults) {
+      console.log("\nUpdated searchResults:\n", searchResults);
+      var pgnum = -1;
+
+      if (searchResults['excerpts'] != null) {
+        searchResults['excerpts'].map(item => {
+          console.log("\n\n");
+          console.log(item);
+          console.log("\n\n");
+          if ('pages' in item) {
+            pgnum = parseInt(item.pages);
+            console.log("ACHOU");
+            console.log(item.pages);
+          }
+        });
+
+        console.log(pgnum);
+
+        if (pgnum != -1)
+          setpagesnum(<Text style={styles.pagesText}>Número de Páginas: {pgnum}</Text>);
+      }
+
+      if (searchResults['subjects'] != null)
+        setSubList(<SubjectsBox subjectsArray={searchResults['subjects']} />)
+
+      if (searchResults['description'])
+        setdescriptionBox(<DescriptionBox descriptionString={searchResults['description']} />)
+
+      if (searchResults['authors'])
+        console.log(searchResults['authors']);
+      setauthorBox(<AuthorBox authorsArray={searchResults['authors']} />)
+    }
+
+  }, [searchResults]);
+
 
   const [bioBox, setbioBox] = useState(<View />);
 
@@ -41,9 +115,9 @@ export default function AuthorPage({ route, navigator }) {
             <Text></Text>
           </View>
           <ScrollView>
-          <View style={{ flex: 1, flexDirection: 'row' }}>
-            {bioBox}
-          </View>
+            <View style={{ flex: 1, flexDirection: 'row' }}>
+              {bioBox}
+            </View>
 
           </ScrollView>
 
